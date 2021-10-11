5f1876f470e56f35b42cf44815d875c8ca11d69c
Boris Alennikov <AlennikovBoris@gmail.com>
2021-10-11 17:39:50 +0300

    Lab--4 test1

---
+++ cmake-build-debug/CMakeFiles/Lab__4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab__4.dir/main.cpp.obj"
+  "Lab__4.exe"
+  "Lab__4.exe.manifest"
+  "Lab__4.lib"
+  "Lab__4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab__4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -22,20 +22,40 @@ int Task1(char s1[255],char s2[255]){
             printf("substring at index i = %d \n", i);
             break;
         }
+        if (j != strlen(s2)){
+            printf("Symbol is not found\n");
+            break;
+        }
     }
     return 0;
 }
 
-void Task2(char s1[255],int count){
-    count++;
-    for (int i = 0; i < strlen(s1); i++){
-        if (s1[i] == ' '){
-            count++;
-        }
-    }
-    cout <<"Number of words: "<< count;
-}
+void Task2(char s1[255]){
+    int slovo = 0, count = 0;
+    int i = 0;
+    int a = '65';
+    int b = '90';
+    int c = '97';
+    int d = '122';
 
+    while(a<=s1[i]<=b && c<=s1[i]<=d){
+        while (s1[i] == ' ' && s1[i] != '\0'){
+            i++;
+        }
+        while (s1[i] != '\0') {
+            if (s1[i] != ' ' && slovo == 0){
+                slovo = 1;
+                count++;
+            }
+            else if (s1[i] == ' '){
+                slovo = 0;
+            }
+            i++;
+        }
+        cout << "Number of words: " << count;
+        exit(1);
+    }
+}
 
 
 int main() {
@@ -45,10 +65,8 @@ int main() {
     char s2[255];
     cout << "Enter word or symbol for search: ";
     cin.getline(s2, 255);
-    int count = 0;
 
     Task1(s1,s2);
-    Task2(s1,count);
-
+    Task2(s1);
 
 }
