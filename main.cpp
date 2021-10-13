ca4a68c41f1a0a8d9f4efe9623b9a46525a3f947
Boris Alennikov <AlennikovBoris@gmail.com>
2021-10-13 22:06:18 +0300

    Lab--4 final

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int Task1(char s1[255],char s2[255]){
+int Task1(char *s1,char *s2){
     int i;
     int n = strlen(s1) - strlen(s2);
     while (strlen(s1) < strlen(s2)){
@@ -19,44 +19,65 @@ int Task1(char s1[255],char s2[255]){
             }
         }
         if (j == strlen(s2)){
-            printf("substring at index i = %d \n", i);
-            break;
-        }
-        if (j != strlen(s2)){
-            printf("Symbol is not found\n");
+            cout << "first meet ukaz  = "<< &i;
             break;
         }
     }
     return 0;
 }
 
-void Task2(char s1[255]){
-    int slovo = 0, count = 0;
-    int i = 0;
-    int a = '65';
-    int b = '90';
-    int c = '97';
-    int d = '122';
 
-    while(a<=s1[i]<=b && c<=s1[i]<=d){
-        while (s1[i] == ' ' && s1[i] != '\0'){
-            i++;
-        }
-        while (s1[i] != '\0') {
-            if (s1[i] != ' ' && slovo == 0){
-                slovo = 1;
-                count++;
-            }
-            else if (s1[i] == ' '){
-                slovo = 0;
-            }
-            i++;
-        }
-        cout << "Number of words: " << count;
-        exit(1);
-    }
+bool isVowel(char a) {
+    if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U')
+        return true;
+    return false;
+}
+bool HalfOfLettersInTheWordAreVowels(char* str, int begin, int end) {
+    int length = end - begin + 1;
+    int num = 0;
+    for (int i = begin; i <= end; i++)
+        if (isVowel(str[i]))
+            num++;
+    return length == 2*num;
+}
+bool doesConsistOnlyOfLatin(char* str, int begin, int end) {
+    if (begin > end)
+        return false;
+
+    for (int i = begin; i <= end; ++i)
+        if (str[i] < 'a' || str[i] > 'z')
+            if (str[i] < 'A' || str[i] > 'Z')
+                return false;
+    return true;
 }
 
+int strlength(char* s1) {
+    int i = 0;
+    while (s1[i] != '\0') { i++; }
+    return i + 1;
+}
+
+void Task2(char* s1) {
+    int begin = 0;
+    int end = 0;
+    int numberVow = 0;
+    int num = 0;
+    int length = strlength(s1);
+
+    for(int i = 0; i < length; ++i) {
+        if ((s1[i] == ' ') || (s1[i] == '\0')) {
+            end = i - 1;
+            if (doesConsistOnlyOfLatin(s1, begin, end)) {
+                num++;
+                if (HalfOfLettersInTheWordAreVowels(s1, begin, end))
+                    numberVow++;
+            }
+            begin = i + 1;
+        }
+    }
+    cout << "\nThe number of words: " << num;
+    cout << "\nThe number of words where the half of letters are vowel: " << numberVow;
+}
 
 int main() {
     char s1[255];
